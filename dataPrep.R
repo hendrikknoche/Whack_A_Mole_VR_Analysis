415b2b3bcfc814dcba0456d42c6bd2ec7ac26aff
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-05-13 15:10:49 +0200

    data prep import, merge, write

---
+++ dataPrep.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(plyr)
+#data import and merge
+setwd("./logData/")
+
+df1<-list.files(pattern = "^eval.*.csv") %>% 
+  map_df(~read_csv(.))
+
+df2<-list.files(pattern = "^sample.*.csv") %>% 
+  map_df(~read_csv(.))
+df<-rbind.fill(df1,df2)%>%arrange(Framecount)    
+write_csv(df,"out.csv")
